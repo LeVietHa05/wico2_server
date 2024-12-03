8075f8a7cbe0dd46d647042b80e151717821e88e
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-12-03 15:46:40 +0700

    add chart

---
+++ data.js
@@ -0,0 +1,34 @@
+let data = [
+    {
+        "id": 1,
+        "time": "1/12/2024",
+        "data": {
+            "temp": 26,
+            "humi": 60,
+            "dust": 14,
+            "aqi": 60
+        }
+    },
+    {
+        "id": 2,
+        "time": "2/12/2024",
+        "data": {
+            "temp": 28,
+            "humi": 55,
+            "dust": 12,
+            "aqi": 50
+        },
+    },
+    {
+        "id": 3,
+        "time": "3/12/2024",
+        "data": {
+            "temp": 25,
+            "humi": 65,
+            "dust": 15,
+            "aqi": 70
+        }
+    }
+]
+
+module.exports = data
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var router = express.Router();
-
+let data = require('../data.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.sendFile("/views/index.html", { root: "./public" })
 });
+
+
+router.get("/getchartdata", (req, res) => {
+  res.json(data).status(200);
+})
+
+router.post("/updatechartdata", (req, res) => {
+  try {
+    let { temp, humi, dust, aqi } = req.body;
+    if (temp && humi && aqi) {
+      data.push({
+        "id": data.length + 1,
+        "time": new Date.toLocaleString('vi-VN'),
+        "data": {
+          "temp": Number(temp).toFixed(2),
+          "humi": Number(humi).toFixed(2),
+          "dust": Number(dust).toFixed(2),
+          "aqi": Number(aqi).toFixed(2)
+        }
+      })
+    }
+    res.json({ "message": "update data successfully" }).status(200);
+  }
+  catch (err) {
+    console.log(err);
+    res.send("err").status(400)
+  }
+})
 
 module.exports = router;
